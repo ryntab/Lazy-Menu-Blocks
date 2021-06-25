0d4fa740bbc509eb4205230aaed71b02c8652cbb
ryntab <57367794+ryntab@users.noreply.github.com>
2021-06-25 04:40:09 -0400

    Refactored JS

--- assets/js/actions.js
+++ assets/js/actions.js
@@ -2,12 +2,10 @@
     $(document).ready(function () {
         $('.menu-item-has-block').mouseenter(function hovering(){
             if ($(this).hasClass('loaded')) return;
-            fetch(`${window.location.origin}/wp-json/lazyMenu/UX/block/${$(this).find('.nav-top-link').data('block')}`)
+            fetch(`${window.location.origin}/wp-json/lazyMenu/UX/block/${$(this).find('.so-lazy').data('block')}`)
             .then(response => response.json())
             .then(data => {
-                $(this).addClass('loaded');
-                $(this).find('.sub-menu').html(data.block);
-                $(this).find('.bg-loaded').remove();
+                $(this).addClass('loaded').find('.sub-menu').html(data.block).find('.bg-loaded').remove();
             })
         });
     });
